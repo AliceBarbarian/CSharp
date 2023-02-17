a5d027a51ebd033698fb3e8e5d03e84ca634be61
Alice Yurchenko <aliceyuri69@gmail.com>
2023-02-18 01:18:30 +0300

    Седьмая задача

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 7
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8 */
+
+
+Console.WriteLine("Введите трехзначное число : ");
+string input = Console.ReadLine();
+if (input.Length == 3){
+    int number = Convert.ToInt32(input);
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последняя цифра : {lastDigit}");
+}
+else (
+    Console.WriteLine("Ошибка! Введите трехзначное число.");
+)
+
