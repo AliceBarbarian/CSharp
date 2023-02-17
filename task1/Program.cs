1d676809e771fbcef72ee6614ba844c94e80d675
Alice Yurchenko <aliceyuri69@gmail.com>
2023-02-17 20:01:05 +0300

    Первая задача

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 1.
+
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое квадратом второго.
+
+a=25; b=55-> да
+
+a=2, b = 10 нет
+
+a=9, b=-3 -> да
+
+a=-3 b = 9 -> нет */
+
+Console.Write("Введите первое чиcло: ");
+int firstnumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе чиcло: ");
+int secondnumber = int.Parse(Console.ReadLine()!);
+int sqr = secondnumber * secondnumber ;
+if
+    (sqr == firstnumber)
+        Console.WriteLine("Первое число является квадратом второго");
+else
+    Console.WriteLine("Первое число не является квадратом второго"); 
+
