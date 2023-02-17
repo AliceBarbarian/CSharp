78c45474a8f4d8256a97619e7fe0a852520f5d87
Alice Yurchenko <aliceyuri69@gmail.com>
2023-02-17 19:32:40 +0300

    решила три задачи

--- task0/Program.cs
+++ task0/Program.cs
@@ -68,4 +68,5 @@ Console.WriteLine("Введите число от 1 до 7: ");
                 default:
                 Console.WriteLine("Ошибка. Пожалуйста, введите число от 1 до 7.");
                 break;
-             };
+             }; */
+
