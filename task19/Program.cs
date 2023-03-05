66971735997aa00b178e5975022aad358da3722b
AliceBarbarian <aliceyuri69@gmail.com>
2023-03-06 02:31:46 +0300

    Правки

--- task19/Program.cs
+++ task19/Program.cs
@@ -20,20 +20,17 @@ class Program
         Console.Write("Введите пятизначное число: ");
         int num = int.Parse(Console.ReadLine());
 
-        // check if the number is a five-digit number
         if (num < 10000 || num > 99999)
         {
             Console.WriteLine("Ошибка! Введите пятизначное число.");
         }
         else
         {
-            // extract digits from the number
             int digit1 = num / 10000;
             int digit2 = (num / 1000) % 10;
             int digit4 = (num / 10) % 10;
             int digit5 = num % 10;
 
-            // check if the number is a palindrome
             if (digit1 == digit5 && digit2 == digit4)
             {
                 Console.WriteLine(num + " да ");
