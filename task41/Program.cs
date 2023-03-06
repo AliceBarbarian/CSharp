2854bf5ded282bd029cd2ccd160907d6e281858e
AliceBarbarian <aliceyuri69@gmail.com>
2023-03-07 01:10:34 +0300

    Решила 41 и 43 задачу

---
+++ task41/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+//Console.Write("Сколько чисел вы хотите ввести? ");
+
+
+//int num = Convert.ToInt32(Console.ReadLine);
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Узнаем сколько чисел хочет ввести пользователь
+        Console.WriteLine("Сколько чисел вы хотите ввести? ");
+        int numCount = Convert.ToInt32(Console.ReadLine());
+
+        // Счётчик положительных чисел
+        int numPlusCount = 0;
+
+        // Создаем массив из М чисел, наполняем его и считаем положительные числа
+        for (int i = 0; i < numCount; i++)
+        {
+            Console.WriteLine($"Введите число {i+1}: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                numPlusCount++;
+            }
+        }
+        
+        // Вывод
+        Console.Write($"Вы ввели {numPlusCount} положительных чисел!");
+        Console.ReadKey();
+    }
+};
---
+++ task43/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Принимаем от пользователя числа
+        Console.Write("Введите первое число: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите третье число: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите четвертое число: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        // Проверяем не парралельны ли прямые, если нет – расчёт
+        if (b1 == b2)
+        {
+            Console.Write("Ошибка! Линии парралельны.");
+        }
+        else
+        {
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * x + b1;
+
+            Console.WriteLine($"Точка пересечения: ({x}, {y}).");
+        }
+        Console.ReadKey();
+    }
+};
